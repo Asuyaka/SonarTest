7412f9830ad058975eef38bdd493a2e3a434504a
Asuyaka <1652145372.@qq.com>
2023-08-24 09:44:27 +0800

    1111

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <MenuBar></MenuBar>
+</template>
+<script>
+import MenuBar from "@/components/MenuBar.vue";
+
+export default {
+  name: 'App',
+  components: {
+      MenuBar
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
